0d4b2524f88c5085a587cc0bb0d805c9bae28211
Tolamo Butale <tolamografesx@gmail.com>
2025-11-18 08:36:28 +0200

    login edit

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ const Navbar = () => {
         <h1 className="text-base font-bold md:text-2xl">Bathusi AI</h1>
       </div>
       {!user ?
-        <Link href={'/dashboard/login'}>
+        <Link href={'/dashboard'}>
           <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
             Login
           </button></Link>:
